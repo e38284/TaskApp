f997ef65ca22819a35eaa4bfbeae62625e416e43
祐太郎萩原 <s21161062@gmail.com>
2019-03-30 01:54:19 +0900

    Initial Commit

---
+++ TaskApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TaskApp
+//
+//  Created by 萩原祐太郎 on 2019/03/30.
+//  Copyright © 2019 Yutaro_Hagiwara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
